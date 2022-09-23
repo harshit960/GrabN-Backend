a787b961d6f63d733b084619a634a6b39e88c0fe
harshit960 <raj.harshit960@gmail.com>
2022-09-23 18:19:48 +0530

    2.3

--- myntra.py
+++ myntra.py
@@ -14,7 +14,7 @@ def getMynta(keyword):
     option.add_argument("--no-sandbox")
     option.add_argument(f'user-agent={user_agent}')
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=option)
-    option.headless=True
+    #option.headless=True
     #option.add_argument('--headless')
     #option.add_argument(f'user-agent={user_agent}')
     #option.add_argument("--ignore-certificate-error")
